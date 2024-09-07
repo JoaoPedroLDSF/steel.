a70c00cd49c2377bd40eff30287211e9c1fc0fb8
JoaoPedroLDSF <166071498+JoaoPedroLDSF@users.noreply.github.com>
2024-09-07 04:21:45 -0300

    Add files via upload

---
+++ pastavs/dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+      titulo: "Dialga",
+      descricao: "o Pokémon do tempo, é uma criatura lendária do tipo Aço e Dragão, introduzida na quarta geração de Pokémon. Conhecido por seu poder de controlar o fluxo do tempo, Dialga é frequentemente retratado como uma divindade em diversas regiões do mundo Pokémon.",
+      link: "https://bulbapedia.bulbagarden.net/wiki/Dialga_(Pokémon)" 
+    },
+    {
+      titulo: "Jirachi",
+      descricao: "o Pokémon que realiza desejos, é uma criatura lendária do tipo Aço e Psíquico, introduzida na terceira geração de Pokémon. Conhecido por seu poder de realizar qualquer desejo, Jirachi é frequentemente procurado por treinadores que desejam obter vantagens em batalhas.",
+      link: "https://bulbapedia.bulbagarden.net/wiki/Jirachi_(Pokémon)"
+    },
+    {
+      titulo: "Lucario",
+      descricao: "o Pokémon Aurórico, é uma criatura lendária do tipo Lutador e Aço, introduzida na quarta geração de Pokémon. Conhecido por sua capacidade de sentir as auras de outros seres e prever seus movimentos, Lucario é um dos Pokémon mais populares entre os treinadores.",
+      link: "https://bulbapedia.bulbagarden.net/wiki/Lucario_(Pokémon)"
+    }
+  ];
